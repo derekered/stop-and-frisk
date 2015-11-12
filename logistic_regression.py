c110aa5b1ee2de04e42e1dd44ee53b7ea0a416c5
Derek Cutting <derekcutting@gmail.com>
2015-11-11 21:30:46 -0500

    consolodated physical force, added precincts to model

--- logistic_regression.py
+++ logistic_regression.py
@@ -9,8 +9,7 @@ import numpy as np
 df = pd.read_csv("CleanData.csv", low_memory=False)
 df.columns = ["pct", "datestop", "timestop", "inout", "trhsloc", "typeofid", "explnstp", "othpers", "arstmade",
               "offunif", "officrid", "frisked", "searched", "contrabn", "pistol", "riflshot", "asltweap", "knifcuti",
-              "machgun", "othrweap", "pf_hands", "pf_wall", "pf_grnd", "pf_drwep", "pf_ptwep", "pf_baton", "pf_hcuff",
-              "pf_pepsp", "pf_other", "rf_vcrim", "rf_othsw", "ac_proxm", "rf_attir", "cs_objcs", "cs_descr",
+              "machgun", "othrweap", "forceused", "rf_vcrim", "rf_othsw", "ac_proxm", "rf_attir", "cs_objcs", "cs_descr",
               "cs_casng", "cs_lkout", "rf_vcact", "cs_cloth", "cs_drgtr", "ac_evasv", "ac_assoc", "cs_furtv",
               "rf_rfcmp", "ac_cgdir", "rf_verbl", "cs_vcrim", "cs_bulge", "cs_other", "ac_incid", "ac_time", "rf_knowl",
               "ac_stsnd", "ac_other", "sb_hdobj", "sb_outln", "sb_admis", "sb_other", "rf_furt", "rf_bulg", "offverb",
@@ -27,13 +26,19 @@ df.columns = ["pct", "datestop", "timestop", "inout", "trhsloc", "typeofid", "ex
 # dummy_inout = pd.get_dummies(df['inout'], prefix='inout')
 # dummy_trhsloc = pd.get_dummies(df['trhsloc'], prefix='trhsloc')
 dummy_race = pd.get_dummies(df['race'], prefix='race')
+dummy_sex = pd.get_dummies(df['sex'], prefix='sex')
+dummy_pct = pd.get_dummies(df['pct'], prefix='pct')
+# dummy_ = pd.get_dummies(df[''], prefix='')
+# dummy_ = pd.get_dummies(df[''], prefix='')
 # dummy_ = pd.get_dummies(df[''], prefix='')
 
+
 # print dummy_race.head()
-# print dummy_race['race_A']
+# print dummy_pct.head()
+
 
 # create a clean data frame for the regression
-cols_to_keep = ["frisked", "contrabn"]
+cols_to_keep = ["frisked", "searched", "contrabn", "othpers", "forceused"]
 
 # cols_to_keep = ["explnstp", "othpers", "arstmade",
 #               "offunif", "officrid", "frisked", "searched", "contrabn", "pistol", "riflshot", "asltweap", "knifcuti",
@@ -44,8 +49,13 @@ cols_to_keep = ["frisked", "contrabn"]
 #               "ac_stsnd", "ac_other", "sb_hdobj", "sb_outln", "sb_admis", "sb_other", "rf_furt", "rf_bulg", "offverb",
 #               "offshld"]
 
-data = df[cols_to_keep].join(dummy_race.ix[:, :'race_U'])
-print data.head()
+# data = df[cols_to_keep].join(dummy_race.ix[:, :'race_W'])
+# data = data.join(dummy_sex.ix[:, :'sex_1'])
+
+data = df[cols_to_keep].join(dummy_race['race_B'])
+data = data.join(dummy_sex['sex_0'])
+data = data.join(dummy_pct.ix[:, :'pct_122'])
+
 
 # manually add the intercept
 data['intercept'] = 1.0
@@ -54,9 +64,9 @@ data['intercept'] = 1.0
 train_cols = data.columns[1:]
 
 # define model with variable to predict
-logit = sm.Logit(data['frisked'], data[train_cols], missing='drop')
+logit = sm.Logit(data['frisked'], data[train_cols])
 
 # fit the model
-result = logit.fit(maxiter=1000)
+result = logit.fit()
 
 print result.summary()
